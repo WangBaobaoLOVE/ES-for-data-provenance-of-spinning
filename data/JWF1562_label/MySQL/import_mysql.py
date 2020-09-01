09c6fc769bbe6eb2d8a3503be2c91f0e84c07fd4
WangBaoBaoLOVE <wbb392797665@sina.com>
2020-09-01 19:12:52 +0800

    get size of data with label stored mysql

---
+++ data/JWF1562_label/MySQL/import_mysql.py
@@ -0,0 +1,45 @@
+import pymysql
+import pandas as pd
+ 
+# 打开数据库连接
+db = pymysql.connect("localhost","root","","mashan" )
+ 
+# 使用cursor()方法获取操作游标 
+cursor = db.cursor()
+
+tables = ['xishalabel1','xishalabel2','xishalabel3','xishalabel4','xishalabel5','xishalabel6','xishalabel7','xishalabel8','xishalabel9','xishalabel10','xishalabel11','xishalabel12']
+nums = {'xishalabel1':1,'xishalabel2':2,'xishalabel3':3,'xishalabel4':4,'xishalabel5':5,'xishalabel6':6,'xishalabel7':7,'xishalabel8':8,'xishalabel9':9,'xishalabel10':10,'xishalabel11':11,'xishalabel12':12}
+data = pd.read_csv('../../JWF1562_origin/MySQL/JWF1562.csv')
+
+
+columns_name = 'before_record_id,'
+for column in data.columns:
+    columns_name = columns_name + column + ','
+columns_name = columns_name.strip(',')
+
+
+for table in tables:
+    for cpnum in range(10000):
+        for num in range(nums[table]):
+            values = '500, \'2019-11-30 11:58:47\','
+            for i in range(1,data.shape[1]):
+                values = values + str(data.iloc[cpnum,i]) + ','
+            values = values.strip(',')
+            values = values.replace('nan','0')
+
+            print(table+'-'+str(cpnum)+'-'+str(num))
+            # SQL 插入语句
+            sql = "INSERT INTO {}({}) VALUES ({})".format(table, columns_name, values)
+            try:
+                # 执行sql语句
+                cursor.execute(sql)
+                # 提交到数据库执行
+                db.commit()
+            except:
+                # 如果发生错误则回滚
+                print('error')
+                db.rollback()
+
+# 关闭数据库连接
+db.close()
+
---
+++ data/JWF1562_label/MySQL/size_store.sql
@@ -0,0 +1,60 @@
+use information_schema;
+select data_length,index_length
+    from tables where
+    table_schema='mashan'
+    and table_name = 'xishalabel1';
+
+select data_length,index_length
+    from tables where
+    table_schema='mashan'
+    and table_name = 'xishalabel2';
+
+select data_length,index_length
+    from tables where
+    table_schema='mashan'
+    and table_name = 'xishalabel3';
+
+select data_length,index_length
+    from tables where
+    table_schema='mashan'
+    and table_name = 'xishalabel4';
+
+select data_length,index_length
+    from tables where
+    table_schema='mashan'
+    and table_name = 'xishalabel5';
+
+select data_length,index_length
+    from tables where
+    table_schema='mashan'
+    and table_name = 'xishalabel6';
+
+select data_length,index_length
+    from tables where
+    table_schema='mashan'
+    and table_name = 'xishalabel7';
+
+select data_length,index_length
+    from tables where
+    table_schema='mashan'
+    and table_name = 'xishalabel8';
+
+select data_length,index_length
+    from tables where
+    table_schema='mashan'
+    and table_name = 'xishalabel9';
+
+select data_length,index_length
+    from tables where
+    table_schema='mashan'
+    and table_name = 'xishalabel10';
+
+select data_length,index_length
+    from tables where
+    table_schema='mashan'
+    and table_name = 'xishalabel11';
+
+select data_length,index_length
+    from tables where
+    table_schema='mashan'
+    and table_name = 'xishalabel12';
