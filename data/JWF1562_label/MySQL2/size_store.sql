6c04155619a55598dde5ce58ce77e54fc7a05374
WangBaoBaoLOVE <wbb392797665@sina.com>
2020-09-02 13:49:57 +0800

    get size

---
+++ data/JWF1562_label/MySQL2/size_store.sql
@@ -0,0 +1,60 @@
+use information_schema;
+select data_length,index_length
+    from tables where
+    table_schema='mashan'
+    and table_name = 'cusha_to_xisha1';
+
+select data_length,index_length
+    from tables where
+    table_schema='mashan'
+    and table_name = 'cusha_to_xisha2';
+
+select data_length,index_length
+    from tables where
+    table_schema='mashan'
+    and table_name = 'cusha_to_xisha3';
+
+select data_length,index_length
+    from tables where
+    table_schema='mashan'
+    and table_name = 'cusha_to_xisha4';
+
+select data_length,index_length
+    from tables where
+    table_schema='mashan'
+    and table_name = 'cusha_to_xisha5';
+
+select data_length,index_length
+    from tables where
+    table_schema='mashan'
+    and table_name = 'cusha_to_xisha6';
+
+select data_length,index_length
+    from tables where
+    table_schema='mashan'
+    and table_name = 'cusha_to_xisha7';
+
+select data_length,index_length
+    from tables where
+    table_schema='mashan'
+    and table_name = 'cusha_to_xisha8';
+
+select data_length,index_length
+    from tables where
+    table_schema='mashan'
+    and table_name = 'cusha_to_xisha9';
+
+select data_length,index_length
+    from tables where
+    table_schema='mashan'
+    and table_name = 'cusha_to_xisha10';
+
+select data_length,index_length
+    from tables where
+    table_schema='mashan'
+    and table_name = 'cusha_to_xisha11';
+
+select data_length,index_length
+    from tables where
+    table_schema='mashan'
+    and table_name = 'cusha_to_xisha12';
